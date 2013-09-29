cf6112c18bd19ab7c9291ffae61d304568692075
Fullplate <michaelharmer.au@gmail.com>
2013-09-29 20:00:44 +1000

    .

--- results.php
+++ results.php
@@ -15,8 +15,7 @@ if (isSet($_POST["artist_search"]))
 	$stmt = $pdo->prepare('
 		SELECT artist, album, rating
 		FROM ratings
-		WHERE artist LIKE :search
-		LIMIT 5');	
+		WHERE artist LIKE :search');	
 	$stmt->execute(array(':search' => $_POST["artist_search"]));
 	
 	$results = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -30,8 +29,7 @@ else if (isSet($_POST["browse"]))
 {
 	$stmt = $pdo->prepare('
 		SELECT *
-		FROM ratings
-		LIMIT 5');
+		FROM ratings');
 	$stmt->execute();
 	
 	$results = $stmt->fetchAll(PDO::FETCH_OBJ);
